d96e9ab54b3337f07ed4ff58cf247a0fec685587
Koichi.Shimomura <unieye51@gmail.com>
2022-07-21 12:04:15 +0900

    3-22 Adding Bootstrap Globally

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+	meta: {
+		link: [
+			{
+				rel: "stylesheet",
+				href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+			}
+		]
+	}
+})
