96c046823e9901cf28d9d10779560339db4a9cb3
Koichi.Shimomura <unieye51@gmail.com>
2022-07-25 09:58:45 +0900

    5-33 Option 1: Using Components

--- pages/restaurants/index.vue
+++ pages/restaurants/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <Html :lang="dynamic > 50 ? 'en-GB' : 'en-US'">
+      <Head>
+        <Title>Top 5O Restaurants in the World</Title>
+        <Meta name="description" content="My page is cool" />
+        <Link
+          rel="preload"
+          href="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png"
+          as="script"
+        />
+      </Head>
+    </Html>
     <NuxtLayout name="custom">
       <div class="container">
         <RestaurantTable />
